d006c416715713ae519537b868b3b9b61fd42fdc
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-06-18 13:48:30 +0900

    Create Pachislot_DataCounter
    データカウンタプログラムを新規登録

---
+++ Pachislot_DataCounter/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Pachislot_DataCounter.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace Pachislot_DataCounter
+{
+        /// <summary>
+        /// Interaction logic for App.xaml
+        /// </summary>
+        public partial class App
+        {
+                /// <summary>
+                /// Prismフレームワークで自動生成されるメソッド
+                /// アプリ開始時に起動するWindowを設定する
+                /// </summary>
+                /// <returns>MainWindowのType</returns>
+                protected override Window CreateShell()
+                {
+                        return Container.Resolve<MainWindow>();
+                }
+
+                /// <summary>
+                /// ViewとViewModelの関連付けを登録する
+                /// </summary>
+                /// <param name="containerRegistry"></param>
+                protected override void RegisterTypes(IContainerRegistry containerRegistry)
+                {
+
+                }
+        }
+}
